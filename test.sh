fa35f79e344a0f910afc32134678f7e297d95c52
PoliEcho <poliecho@pupes.org>
2025-08-17 15:14:07 +0200

    addtions to test script

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cp /usr/share/edk2/x64/OVMF_VARS.4m.fd ./my_ovmf_vars.fd
+if [ $# -ge 1 ]; then
+  OVMF_CODE_PATH=$1
+else 
+  OVMF_CODE_PATH=/usr/share/edk2/x64/OVMF_CODE.4m.fd
+fi
 
 qemu-system-x86_64 \
   -machine type=q35,accel=kvm \
-  -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
-  -drive if=pflash,format=raw,file=./my_ovmf_vars.fd \
+  -drive if=pflash,format=raw,readonly=on,file="$OVMF_CODE_PATH" \
   -hda fat:rw:build \
   -boot order=c \
   -smp 4 \
   -s \
   -serial mon:stdio
-
-
-rm ./my_ovmf_vars.fd
