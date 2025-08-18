54a54de423d1de399af766d0b804e2333563ef8b
PoliEcho <poliecho@pupes.org>
2025-08-18 21:05:04 +0200

    enhance build script

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ xxd -i assets/rocket.bmp > src/UEFI_fireworks/rocket.c
 if [ "$1" == "debug" ]; then
     build -a X64 -t GCC5 -p UEFI_fireworks.dsc -b DEBUG
 else
-    build -a X64 -t GCC5 -p UEFI_fireworks.dsc
+    build -a X64 -t GCC5 -p UEFI_fireworks.dsc -b RELEASE
 fi
 mkdir -p build/EFI/BOOT/
+if [ "$1" == "debug" ]; then
 cp edk2/Build/UEFI_fireworks/DEBUG_GCC5/X64/UEFI_fireworks.efi build/
 cp edk2/Build/UEFI_fireworks/DEBUG_GCC5/X64/UEFI_fireworks.efi build/EFI/BOOT/BOOTX64.EFI
+else
+cp edk2/Build/UEFI_fireworks/RELEASE_GCC5/X64/UEFI_fireworks.efi build/
+cp edk2/Build/UEFI_fireworks/RELEASE_GCC5/X64/UEFI_fireworks.efi build/EFI/BOOT/BOOTX64.EFI
+fi
